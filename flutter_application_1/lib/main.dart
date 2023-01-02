40d066fc23a5e43cb072c009ec9fbdaf71227d66
aleksandar <aca.djordjevic1113@gmail.com>
2023-01-01 23:38:32 -0800

    added go router to handle routes

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,280 +1,38 @@
-// import 'package:flutter/material.dart';
-// import 'package:qr_code_scanner/qr_code_scanner.dart';
-// import 'dart:developer';
-// import 'dart:io';
-// import 'package:flutter/foundation.dart' show describeEnum;
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Gane',
-//       theme: ThemeData(
-//         primarySwatch: Colors.green,
-//       ),
-//       home: const MyHomePage(title: 'Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'sa ima:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add_a_photo_outlined),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
-// ///QR Code
-// void main() => runApp(const MaterialApp(home: MyHome()));
-
-// class MyHome extends StatelessWidget {
-//   const MyHome({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Flutter Demo Home Page')),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () {
-//             Navigator.of(context).push(MaterialPageRoute(
-//               builder: (context) => const QRViewExample(),
-//             ));
-//           },
-//           child: const Text('qrView'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class QRViewExample extends StatefulWidget {
-//   const QRViewExample({Key? key}) : super(key: key);
-
-//   @override
-//   State<StatefulWidget> createState() => _QRViewExampleState();
-// }
-
-// class _QRViewExampleState extends State<QRViewExample> {
-//   Barcode? result;
-//   QRViewController? controller;
-//   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-//   // In order to get hot reload to work we need to pause the camera if the platform
-//   // is android, or resume the camera if the platform is iOS.
-//   @override
-//   void reassemble() {
-//     super.reassemble();
-//     if (Platform.isAndroid) {
-//       controller!.pauseCamera();
-//     }
-//     controller!.resumeCamera();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         children: <Widget>[
-//           Expanded(flex: 4, child: _buildQrView(context)),
-//           Expanded(
-//             flex: 1,
-//             child: FittedBox(
-//               fit: BoxFit.contain,
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: <Widget>[
-//                   if (result != null)
-//                     Text(
-//                         'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-//                   else
-//                     const Text('Scan a code'),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     crossAxisAlignment: CrossAxisAlignment.center,
-//                     children: <Widget>[
-//                       Container(
-//                         margin: const EdgeInsets.all(8),
-//                         child: ElevatedButton(
-//                             onPressed: () async {
-//                               await controller?.toggleFlash();
-//                               setState(() {});
-//                             },
-//                             child: FutureBuilder(
-//                               future: controller?.getFlashStatus(),
-//                               builder: (context, snapshot) {
-//                                 return Text('Flash: ${snapshot.data}');
-//                               },
-//                             )),
-//                       ),
-//                       Container(
-//                         margin: const EdgeInsets.all(8),
-//                         child: ElevatedButton(
-//                             onPressed: () async {
-//                               await controller?.flipCamera();
-//                               setState(() {});
-//                             },
-//                             child: FutureBuilder(
-//                               future: controller?.getCameraInfo(),
-//                               builder: (context, snapshot) {
-//                                 if (snapshot.data != null) {
-//                                   return Text(
-//                                       'Camera facing ${describeEnum(snapshot.data!)}');
-//                                 } else {
-//                                   return const Text('loading');
-//                                 }
-//                               },
-//                             )),
-//                       )
-//                     ],
-//                   ),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     crossAxisAlignment: CrossAxisAlignment.center,
-//                     children: <Widget>[
-//                       Container(
-//                         margin: const EdgeInsets.all(8),
-//                         child: ElevatedButton(
-//                           onPressed: () async {
-//                             await controller?.pauseCamera();
-//                           },
-//                           child: const Text('pause',
-//                               style: TextStyle(fontSize: 20)),
-//                         ),
-//                       ),
-//                       Container(
-//                         margin: const EdgeInsets.all(8),
-//                         child: ElevatedButton(
-//                           onPressed: () async {
-//                             await controller?.resumeCamera();
-//                           },
-//                           child: const Text('resume',
-//                               style: TextStyle(fontSize: 20)),
-//                         ),
-//                       )
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _buildQrView(BuildContext context) {
-//     // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-//     var scanArea = (MediaQuery.of(context).size.width < 400 ||
-//             MediaQuery.of(context).size.height < 400)
-//         ? 150.0
-//         : 300.0;
-//     // To ensure the Scanner view is properly sizes after rotation
-//     // we need to listen for Flutter SizeChanged notification and update controller
-//     return QRView(
-//       key: qrKey,
-//       onQRViewCreated: _onQRViewCreated,
-//       overlay: QrScannerOverlayShape(
-//           borderColor: Colors.red,
-//           borderRadius: 10,
-//           borderLength: 30,
-//           borderWidth: 10,
-//           cutOutSize: scanArea),
-//       onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-//     );
-//   }
-
-//   void _onQRViewCreated(QRViewController controller) {
-//     setState(() {
-//       this.controller = controller;
-//     });
-//     controller.scannedDataStream.listen((scanData) {
-//       setState(() {
-//         result = scanData;
-//       });
-//     });
-//   }
-
-//   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-//     log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
-//     if (!p) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         const SnackBar(content: Text('no Permission')),
-//       );
-//     }
-//   }
-
-//   @override
-//   void dispose() {
-//     controller?.dispose();
-//     super.dispose();
-//   }
-// }
-
 ////LEARNING /////
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:developer';
+import 'dart:io';
+import 'package:flutter/foundation.dart' show describeEnum;
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'reader',
+          builder: (BuildContext context, GoRouterState state) {
+            return const QRViewExample();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int count = 0;
+class HomeScreen extends StatelessWidget {
+  /// Constructs a [HomeScreen]
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -341,7 +99,7 @@ class _MyAppState extends State<MyApp> {
               width: 100,
               height: 500,
               child: Text(
-                '$count',
+                '2',
               ),
             ),
             Container(
@@ -363,23 +121,19 @@ class _MyAppState extends State<MyApp> {
         ),
 
         floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.camera),
-            onPressed: () {
-              print('pressed');
-              setState(() {
-                count++;
-              });
-            }),
+          child: Icon(Icons.camera),
+          onPressed: () => context.go('/reader'),
+        ),
 
         bottomNavigationBar: BottomNavigationBar(items: [
           BottomNavigationBarItem(
+            
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.business),
             label: 'Business',
-            
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.toll_outlined),
@@ -391,6 +145,191 @@ class _MyAppState extends State<MyApp> {
           child: Text('Yo!'),
         ),
       ),
+    );
+  }
+}
+
+//QR CODE
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+          Expanded(
+            flex: 1,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  if (result != null)
+                    Text(
+                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                  else
+                    const Text('Scan a code'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.toggleFlash();
+                              setState(() {});
+                            },
+                            child: FutureBuilder(
+                              future: controller?.getFlashStatus(),
+                              builder: (context, snapshot) {
+                                return Text('Flash: ${snapshot.data}');
+                              },
+                            )),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.flipCamera();
+                              setState(() {});
+                            },
+                            child: FutureBuilder(
+                              future: controller?.getCameraInfo(),
+                              builder: (context, snapshot) {
+                                if (snapshot.data != null) {
+                                  return Text(
+                                      'Camera facing ${describeEnum(snapshot.data!)}');
+                                } else {
+                                  return const Text('loading');
+                                }
+                              },
+                            )),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () => context.go('/'),
+                          child: const Text('Home',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.pauseCamera();
+                          },
+                          child: const Text('pause',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.resumeCamera();
+                          },
+                          child: const Text('resume',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
     );
   }
 }
